600f1caf3ee8c60b5d577c67d81ee1e1f6dd55bd
puppypaduk2 <debraooclark@gmail.com>
2018-08-18 14:30:15 +0300

    setup server + express

---
+++ src/server/http/index.js
@@ -0,0 +1,5 @@
+export function main(req, res) {
+  return res.send('TEST');
+}
+
+export default { main };
--- src/server/index.js
+++ src/server/index.js
@@ -1,1 +1,17 @@
-console.log(123);
+import path from 'path';
+import express from 'express';
+import http from 'http';
+import { main } from './http';
+
+const PORT = 3000;
+const app = express();
+const httpServer = http.Server(app);
+
+app.use(express.static(path.join('client')));
+app.get('/', main);
+
+// socket(httpServer);
+
+httpServer.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}!`);
+});
