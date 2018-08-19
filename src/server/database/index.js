0bb4cd840c1dbea1877a40653949ffeb1767404a
puppypaduk2 <debraooclark@gmail.com>
2018-08-20 00:14:51 +0300

    setup connect database

--- src/server/database/index.js
+++ src/server/database/index.js
@@ -1,16 +1,16 @@
-import * as mongoose from 'mongoose';
+import mongoose from 'mongoose';
 
 export default function () {
-  // const { database } = mongoose.connection;
+  const database = mongoose.connection;
   const { error, log } = console;
 
-  // mongoose.connect('mongodb://localhost/findPart');
+  mongoose.connect('mongodb://localhost/findPart');
 
-  // on('error', error.bind(console, 'connection error:'));
+  database.on('error', error.bind(console, 'connection error:'));
 
-  // mongoose.once('open', () => {
-  //   log('Open connect with database!');
-  // });
+  database.once('open', () => {
+    log('Open connect with database!');
+  });
 
-  // return database;
+  return database;
 }
