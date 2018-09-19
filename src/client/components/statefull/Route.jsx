02f4724c80b40ff2f161dcb9f680715a2e804a13
puppypaduk2 <debraooclark@gmail.com>
2018-09-19 22:01:11 +0300

    setup route

--- src/client/components/statefull/Route.jsx
+++ src/client/components/statefull/Route.jsx
@@ -1,13 +1,19 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import pages from '../pages';
 
 class Route extends Component {
   render() {
     const { props } = this;
+
+    console.log(pages);
 
     return (
       <div className="route"></div>
     );
   }
 }
+
+Route.propTypes = {};
 
 export default Route;
