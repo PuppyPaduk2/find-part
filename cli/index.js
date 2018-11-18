cbe7a92ab6a1c2632cc8e4af3b5d9dcc0592ef16
puppypaduk2 <debraooclark@gmail.com>
2018-11-18 16:24:46 +0300

    setup cli

--- cli/index.js
+++ cli/index.js
@@ -1,13 +1,21 @@
 const program = require('commander');
+const fs = require('fs');
+const chalkPipe = require('chalk-pipe');
 
 program
-  .version('0.0.1')
-  .option('-c, --create <path>', 'enable some action');
+  .version('0.0.1');
+
+program
+  .command('create [name]')
+  .description('run create by template functional area')
+  .action((env, options) => {
+    console.log(env, options);
+  })
+  .on('--help', () => {
+    console.log('Name:');
+
+    fs.readdirSync(`${__dirname}/create`)
+      .forEach(fileName => console.log(chalkPipe('cyan')(`  ${fileName}`)));
+  });
 
 program.parse(process.argv);
-
-if (program.create) {
-  require(`./create/${program.create}`);
-} else {
-  program.help();
-}
